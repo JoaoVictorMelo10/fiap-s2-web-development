e26d14f7de6f6c9bfd2b760ff05a28a58e5b9093
João Victor <melo.joaovitor10@gmail.com>
2026-04-13 10:45:11 -0300

    chore: Configurando a estrutura do banco de dados

---
+++ Projetoloja/src/Data/Dados.js
@@ -0,0 +1,52 @@
+const menu=[
+{
+    id:1,
+    titulo:"Teclado",
+    categoria:"gamer",
+    preco:200.00,
+    img:"/src/assets/img.jpg",
+    descricao:"Teclado Gamer de alta tecnologia"
+},
+{
+    id:2,
+    titulo:"Mouse",
+    categoria:"aventura",
+    preco:100.00,
+     img:"/src/assets/img.jpg",
+    descricao:"Rato motorizado top"
+},
+{
+    id:3,
+    titulo:"Cadeira",
+    categoria:"aventura",
+    preco:2000.00,
+     img:"/src/assets/img.jpg",
+    descricao:"Cadeira com massageador"
+},
+{
+    id:4,
+    titulo:"HeadSet",
+    categoria:"gamer",
+    preco:200.00,
+     img:"/src/assets/img.jpg",
+    descricao:"Teclado Gamer de alta tecnologia"
+},
+{
+    id:5,
+    titulo:"Jogo Mario",
+    categoria:"acao",
+    preco:20.00,
+     img:"/src/assets/img.jpg",
+    descricao:"Jogo do mario bros"
+},
+{
+    id:6,
+    titulo:"Jogo do Sonic",
+    categoria:"acao",
+    preco:70.00,
+     img:"/src/assets/img.jpg",
+    descricao:"Jogo voando alto"
+},
+
+]
+export default menu
